52d4489d811619c2366c629f76c66c34dd3d1ba2
jirwincline <jakobinwins@gmail.com>
2019-06-28 16:24:48 -0700

    start on user interface, but run into bug where age in mercury years is '0' when it shouldn't be

--- src/main.js
+++ src/main.js
@@ -5,5 +5,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Person } from './solar-calc.js';
 
 $(document).ready(function () {
-  $()
+  $("form").submit(function(event) {
+    event.preventDefault();
+    let expectancy = '';
+    const user = new Person($('input#name').val(),$('input[name="gender"]:checked').val(),$('input#birthday').val());
+    console.log(user);
+    user.calcAge();
+    user.calcLifeExpectancy();
+    $("#mercury").click(function(){
+      user.calcMercury();
+      expectancy = user.yearsTillDead();
+      $('.result-mercury').append('Your age in Mercury years is: ' + user.newAge);
+      $('.result-mercury').append(expectancy);
+      console.log('here')
+    });
+    $("#venus").click(function(){
+      user.calcVenus();
+      expectancy = user.newYearsTillDead
+
+    });
+    $("#earth").click(function(){
+      user.calcVenus();
+      expectancy = user.yearsTillDead
+
+    });
+    $("#mars").click(function(){
+      user.calcVenus();
+      expectancy = user.newYearsTillDead
+
+    });
+    $("#jupiter").click(function(){
+      user.calcVenus();
+      expectancy = user.newYearsTillDead
+
+    });
+  });
 });
