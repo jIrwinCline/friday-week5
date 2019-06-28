f66958c99076ca922786e0834a730d077ffc54d8
jirwincline <jakobinwins@gmail.com>
2019-06-28 10:13:09 -0700

    add the testing statements

---
+++ spec/solar-calc-test.js
@@ -0,0 +1,42 @@
+import { Triangle } from './../src/triangle.js';
+
+describe('age-calc', function () {
+
+  it('should test whether the mercury age calculator reduces the input age to 24%', function () {
+
+    expect(triangle.side3).not.toEqual(6);
+  });
+
+  it('should test whether the venus age calculator reduces the input age to 62%', function () {
+
+      expect(notTriangle.checkType()).toEqual('not a triangle');
+
+
+    });
+  it('should test whether the mars age calculator increases the input age to 188%', function () {
+
+      expect(notTriangle.checkType()).toEqual('not a triangle');
+
+
+    });
+  it('should test whether the Jupiter age calculator increases the input age to 1186%', function () {
+
+      expect(notTriangle.checkType()).toEqual('not a triangle');
+
+
+    });
+  it('should test if the years till they hit life expectancy will display properly and change for each planets years', function () {
+
+      expect(notTriangle.checkType()).toEqual('not a triangle');
+
+
+    });
+  it('should test if the the output informs users how many years past the life expectancy they have lived', function () {
+
+      expect(notTriangle.checkType()).toEqual('not a triangle');
+
+
+    });
+
+
+});
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-
 import './styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { calc } from './solar-calc.js';
 
 $(document).ready(function () {
-  
+
 });
