7491f975ea4ca4478cfc68e8c2be831f626dc349
jirwincline <jakobinwins@gmail.com>
2019-06-28 12:40:50 -0700

    make the next 3 tests fail

--- spec/solar-calc-spec.js
+++ spec/solar-calc-spec.js
@@ -9,35 +9,35 @@ describe('Person', function () {
   });
 
   it('should test whether the mercury age calculator increases the input age to ~4.16%', function () {
-    expect(user.calcMercury() + 1).toEqual(user.age / .24);
+    expect(user.calcMercury()).toEqual(user.age / .24);
   });
 
   it('should test whether the venus age calculator increase the input age to 1.6%%', function () {
-
+    expect(user.calcVenus()  + 1).toEqual(user.age / .62);
 
 
 
     });
   it('should test whether the mars age calculator decrease the input age to 53.19%', function () {
-
+    expect(user.calcMars() + 1).toEqual(user.age / 1.88);
 
 
 
     });
   it('should test whether the Jupiter age calculator decrease the input age to 8.4%', function () {
-
+    expect(user.calcJupiter() + 1).toEqual(user.age / 11.86);
 
 
 
     });
   it('should test if the years till they hit life expectancy will display properly and change for each planets years', function () {
-
+    // expect().toEqual();
 
 
 
     });
   it('should test if the the output informs users how many years past the life expectancy they have lived', function () {
-
+    // expect().toEqual();
 
 
 
